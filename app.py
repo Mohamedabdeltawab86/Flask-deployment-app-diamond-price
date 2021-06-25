e9c63ada7b0ee097eadc150457b9959d46c31a33
Mohamed Abdeltawab <dr.m.tawab2@gmail.com>
2021-06-25 02:25:43 +0200

    make it better

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('model.h5')
+scaler = joblib.load('scaler.h5')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    catar = request.args.get('catar')
+    cut = request.args.get("cut")
+    depth = request.args.get('depth')
+    table = request.args.get("table")
+    size = request.args.get("size")
+
+    price = model.predict([[catar, cut, depth, table, size ]])
+    return render_template("index.html", price = price)
+
+
+
+if __name__ == "__main__":
+    app.run()
